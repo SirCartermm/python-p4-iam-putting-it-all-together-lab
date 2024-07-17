62c2c124666a26608537afed3a7922dcd1aab163
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-17 17:09:00 +0300

    Modified  recipe.py

--- server/recipe.py
+++ server/recipe.py
@@ -13,3 +13,24 @@ class Recipe(Resource):
                     return jsonify({'id': recipe.id, 'title': recipe.title, 'instructions': recipe.instructions, 'inutes_to_complete': recipe.minutes_to_complete, 'user': {'id': user.id, 'username': user.username, 'image_url': user.image_url, 'bio': user.bio}}), 200
                 return jsonify({'error': 'Recipe not found'}), 404
         return jsonify({'error': 'Unauthorized'}), 401
+
+    def put(self, id):
+        if 'user_id' in session:
+            user_id = session['user_id']
+            user = User.query.get(user_id)
+            if user:
+                recipe = Recipe.query.get(id)
+                if recipe:
+                    data = request.get_json()
+                    if not data:
+                        return jsonify({'error': 'Invalid request'}), 422
+                    recipe.title = data['title']
+                    recipe.instructions = data['instructions']
+                    recipe.minutes_to_complete = data['minutes_to_complete']
+                    try:
+                        db.session.commit()
+                        return jsonify({'id': recipe.id, 'title': recipe.title, 'instructions': recipe.instructions, 'inutes_to_complete': recipe.minutes_to_complete, 'user': {'id': user.id, 'username': user.username, 'image_url': user.image_url, 'bio': user.bio}}), 200
+                    except:
+                        return jsonify({'error': 'Invalid request'}), 422
+                return jsonify({'error': 'Recipe not found'}), 404
+        return jsonify({'error': 'Unauthorized'}), 401
