88cf2106fbffcb1a3b349a7b2946c25353b08a06
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-17 17:08:21 +0300

    Modified  recipe.py

---
+++ server/recipe.py
@@ -0,0 +1,15 @@
+from flask import session, request, jsonify
+from flask_restful import Resource
+from models import Recipe, User
+
+class Recipe(Resource):
+    def get(self, id):
+        if 'user_id' in session:
+            user_id = session['user_id']
+            user = User.query.get(user_id)
+            if user:
+                recipe = Recipe.query.get(id)
+                if recipe:
+                    return jsonify({'id': recipe.id, 'title': recipe.title, 'instructions': recipe.instructions, 'inutes_to_complete': recipe.minutes_to_complete, 'user': {'id': user.id, 'username': user.username, 'image_url': user.image_url, 'bio': user.bio}}), 200
+                return jsonify({'error': 'Recipe not found'}), 404
+        return jsonify({'error': 'Unauthorized'}), 401
