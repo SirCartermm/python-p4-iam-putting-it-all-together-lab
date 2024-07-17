37158d4c76f95dc675319a5bce89c25c83f63f42
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-17 16:42:05 +0300

    modified models.py

---
+++ server/models.py
@@ -0,0 +1,28 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from werkzeug.security import generate_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String, nullable=False)
+    bio = db.Column(db.String, nullable=False)
+    recipes = db.relationship('Recipe', backref='user', lazy=True)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self._password_hash = generate_password_hash(password)
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
